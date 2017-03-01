b171bba046f61d127aa2b487951c91036c47d734
OnyxFlames <jacobhicks1996@hotmail.com>
2017-03-01 17:14:25 -0500

    Added REQUIREMENTS.md Added comments to "Hello world.s" to better explain the code.

--- Hello world.s
+++ Hello world.s
@@ -1,13 +1,17 @@
-LC0:
-    .ascii "Hello world!\0"
+# Hello world.s
+# Prints "Hello world!\n" to STDIN
+# Compile as: "gcc Helloworld.s -o Helloworld.exe" or compiler equivalent
 
-.globl _main
+LC0:
+    .ascii "Hello world!\0" # Load "Hello world!\0" as a value to be later printed
+
+.globl _main                # Define _main as a global, won't compile without.
 
 _main:
-    pushl   %ebp
-    movl	%esp, %ebp
-    movl    $LC0, (%esp)
-    call    _puts
-    xorl    %eax, %eax
-    leave
-    ret
+    pushl   %ebp            # Push the stack base - crashes without this line  
+    movl	%esp, %ebp      # Push the current stack pointer to %ebp
+    movl    $LC0, (%esp)    # Move the line "Hello world!\0" into %esp
+    call    _puts           # Call _puts which will print the value of %esp
+    xorl    %eax, %eax      # Generates when returning 0 at the end of main()
+    leave                   # Crashes without
+    ret                     # Return out of _main
---
+++ REQUIREMENTS.md
@@ -0,0 +1,3 @@
+# Requirements
+This project compiles using MinGW GCC 5.3.0 for Windows 10.
+The assembly generated for learning purposes was generated using GCC 5.3.0 under the -S and -Os flags.
